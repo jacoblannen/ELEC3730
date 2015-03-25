1b73e58d18f391b9e7e500d985bab6a53d9b67fd
jacoblannen <jacob.lannen@gmail.com>
2015-03-25 22:36:57 +1100

    Full code for question 1 of assignment 1
    Contains 2 functions extract_little() and extract_big()
    Needs comments.
    May need to be split into two separate files.

---
+++ Assignment1/question1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "ass1.h"
+
+int extract_little(char* str, int offset, int n)
+{
+	int temp;
+	int power;
+	int output = 0;
+	int i;
+	
+	if(n>sizeof(int)){
+		printf("String larger than maximum size for int (%d bytes)",sizeof(int));
+		output=-1;
+	}else{
+		for(i = 0; i < n; i++){
+			temp = *(str+offset);
+			power=(int)pow((float)256,i);
+			output+= temp*power;
+			offset++;
+		}
+	}
+	return(output);
+}
+
+int extract_big(char* str, int offset, int n)
+{
+	int temp;
+	int power;
+	int output = 0;
+	int i;
+	
+	if(n>sizeof(int)){
+		printf("String larger than maximum size for int (%d bytes)",sizeof(int));
+		output=-1;
+	}else{
+		for(i = 0; i < n; i++){
+			temp = *(str+offset);
+			power=(int)pow((float)256,n-1-i);
+			output+= temp*power;
+			offset++;
+		}
+	}
+	return(output);
+}
