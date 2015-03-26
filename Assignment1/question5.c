366c5759f2bed3e187173decfaaf4db095566c8c
jacoblannen <jacob.lannen@gmail.com>
2015-03-26 18:08:23 +1100

    Create question5.c

---
+++ Assignment1/question5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ass1.h"
+
+char string_parser(char* inp, char* array_of_words[]){
+	char output;
+	int i=0;
+	int j=0;
+	
+	while(inp[i]!=0){
+		
+	}
+	
+	return(output);
+}
