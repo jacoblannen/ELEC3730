22d1a700c4fd27422d4bb6c407f8e0a56a48c2ff
jacoblannen <jacob.lannen@gmail.com>
2015-03-27 04:47:49 +1100

    Added comments

--- Assignment1/question6a.c
+++ Assignment1/question6a.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
-#include <conio.h>
-#include "ass1.h"
+/* Jacob Lannen
+ * 3162100
+ * Assignment 1 question 6a - Program to take keystroke and display hex code of character pressed
+ * 27/3/2015
+ */
 
-int keyboard_codes(void){
-	char c;
+#include <stdio.h>						//Included for printf()
+#include <conio.h>						//Included for getch()
+
+int main(void){
+	char c;								//Char to be displayed
 	
-	while(1){
+	while(c!=0xd&&c!=0x1b){				//Loop until "esc" or "enter" keystroke
 		c = getch();
-		printf("%x\n",c);
+		printf("%x\n",c);				//Display char as hex value
 	}
 		
 	return(0);
