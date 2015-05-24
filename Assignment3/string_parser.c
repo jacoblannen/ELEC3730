8e290eb6f65508a0c2092df7cda6d366ec62ad34
Jacob Lannen <jacob.lannen@gmail.com>
2015-05-25 00:08:32 +1000

    Create string_parser.c

---
+++ Assignment3/string_parser.c
@@ -0,0 +1,54 @@
+/*
+ * string_parser.c
+ *
+ *  Created on: 21/05/2015
+ *      Author: c3162100
+ */
+
+#include "ass3.h"														//Included for function prototypes
+
+char string_parser(char* inp, char* array_of_words[]){
+	char buffer[50];													//Buffer string used to retrieve single word at a time to be placed into array
+	int i=0;															//Loop counters
+	int j=0;
+	int k=0;
+
+	if(inp<0||array_of_words<0){										//Check pointers contain valid memory addresses
+		printf("Please ensure pointers point to valid memory address.\n");
+		return(0);
+	}
+
+	while(inp[i]!=0){													//While loop till end of input string (if input is NULL the loop ends)
+		if(inp[i]!=' '){												//If current char in input string is not a space move it into the buffer and increment indicies
+			buffer[k] = inp[i];
+			i++;
+			k++;
+		}else{
+			buffer[k]='\0';												//If input char is a space null terminate buffer string
+			array_of_words[j] = malloc(k);								//Initialise string to required length and move buffer in.
+			strcpy(array_of_words[j],buffer);
+
+			if(i>0){													//Increment word count ignoring leading space
+				j++;
+			}
+			while(inp[i]==' '){											//Check for multiple spaces
+				i++;
+			}
+
+			k=0;														//Reset buffer index for new word
+		}
+	}
+
+	if(i!=0){															//After end of input string is detected check that it wasn't empty
+		if(inp[i-1]!=' '){												//Ensure last char was not a space
+			buffer[k]='\0';												//Move final buffer into array and increment word count
+			array_of_words[j] = malloc(k);
+			strcpy(array_of_words[j],buffer);
+			j++;
+		}
+	}else{																//Display error if string is empty
+		printf("ERROR: Input string contains starts with NULL.\n");
+	}
+
+	return(j);															//Return wordcount
+}
