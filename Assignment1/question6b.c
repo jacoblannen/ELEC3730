08b564bce811beca2b9599c7ff1c54b16d3c21dd
Jacob Lannen <jacob.lannen@gmail.com>
2016-03-17 15:04:56 +1100

    Update question6b.c

--- Assignment1/question6b.c
+++ Assignment1/question6b.c
@@ -13,11 +13,13 @@ int main()
 	int uart_read=open("/dev/uart_0",O_RDONLY);		//Open UART with a read variable
 	int uart_write=open("/dev/uart_0",O_WRONLY);	//Open UART with a write variable
 	char buffer[100];								//Buffer to recieve/transmit UART data
+	char out[20];
 
 	while(buffer[0]!=0xd&&buffer[0]!=0x1b){			//Loop until "esc" or "enter" keystroke
 		read(uart_read,buffer,1);					//Read from UART
-		write(uart_write,buffer,1);					//Write to UART
 		printf("%c",buffer[0]);						//Display on console
+		sprintf(out,"%c\t%x\r\n",buffer[0],buffer[0]);
+		write(uart_write,out,strlen(out));					//Write to UART
 	}
 
   return (0);
